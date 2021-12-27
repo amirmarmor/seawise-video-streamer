76897cee7178fb02d536ae4b63cd29fa5357cc70
amirma <amirma@radware.com>
2021-12-27 18:14:30 +0200

    change controller ip

--- debug.sh
+++ debug.sh
@@ -4,8 +4,7 @@ sudo rmmod uvcvideo
 sudo modprobe uvcvideo nodrop=1 timeout=1000 quirks=0x80
 
 export VERBOSE=5
-export BEHOST="192.168.1.16"
-export STHOST="192.168.1.16"
+export BEHOST="192.168.1.21"
 echo $BEHOST
 cmd=./start
 $cmd
