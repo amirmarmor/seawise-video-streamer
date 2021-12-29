1eaf5cfbcdaa9614cdceec1e2b6c8aa3ec72ce9b
amirma <amirma@radware.com>
2021-12-29 21:34:37 +0200

    update update

--- debug.sh
+++ debug.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 sudo rmmod uvcvideo
-sudo modprobe uvcvideo nodrop=1 timeout=1000 quirks=0x80
+sudo modprobe uvcvideo nodrop=1 timeout=200 quirks=0x80
 
 export VERBOSE=5
 export BEHOST="192.168.1.16"
-echo $BEHOST
+echo "connecting to " + $BEHOST
 cmd=./start
 $cmd
 
