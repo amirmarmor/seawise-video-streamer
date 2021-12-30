de1a63988bcfe1431d7de2700c84253fe3ad1ae2
amirma <amirma@radware.com>
2021-12-30 10:37:26 +0200

    update

---
+++ debugpi.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+sudo rmmod uvcvideo
+sudo modprobe uvcvideo nodrop=1 timeout=10000 quirks=0x80
+
+export VERBOSE=5
+export BEHOST="192.168.1.16"
+echo "connecting to " + $BEHOST
+cmd=/home/pi/seawise-video-streamer/start
+$cmd
+
+
+now=$(date +"%T")
+echo "[$now] Running"
---
+++ updatepi.sh
@@ -0,0 +1,4 @@
+git reset HEAD --hard
+git pull
+/home/pi/seawise-video-streamer/build.sh
+/home/pi/seawise-video-streamer/debugpi.sh
