f5d60241593dddb979ff37798f4ed671407b2223
amirma <amirma@radware.com>
2022-01-24 11:28:33 +0200

    ip

--- debugpi.sh
+++ debugpi.sh
@@ -4,7 +4,7 @@ sudo rmmod uvcvideo
 sudo modprobe uvcvideo nodrop=1 timeout=10000 quirks=0x80
 
 export VERBOSE=5
-export BEHOST="192.168.0.30" #"192.168.1.11"
+export BEHOST="192.168.10.8" #"192.168.1.11"
 echo "connecting to " + $BEHOST
 cmd=/home/pi/seawise-video-streamer/start
 $cmd
