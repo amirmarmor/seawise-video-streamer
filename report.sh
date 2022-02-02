9ec4e24e6b5a7f774b471d6a707a8bd3a716852c
amirma <amirma@radware.com>
2022-02-02 20:00:33 +0200

    report script

--- report.sh
+++ report.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-BEHOST="192.168.10.8:8080"
+host="192.168.10.8:8080"
+sn="$(cat /proc/cpuinfo | grep Serial | cut -d ' ' -f 2)"
+url="http://$host/report"
+ip="$(/sbin/ip -o -4 addr list wlan0 | awk '{print $4}' | cut -d)"
+
 n=0
 
 until [ "$n" -ge 5 ]
 do
-  url="http://$BEHOST/report"
   echo "attempt $n - $url"
-  curl "$url" && break
+  curl -X POST -H "Content-Type: application/json" -d '{"sn": "'$sn'", "ip": "'$ip'"}' "$url" && break
   n=$((n+1))
   sleep 15
 done
