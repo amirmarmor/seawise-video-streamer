28363e0fccf67e4c9b6ffb7e960a43dc23712355
amirma <amirma@radware.com>
2022-02-06 14:25:25 +0200

    update local

--- debuglocal.sh
+++ debuglocal.sh
@@ -6,7 +6,7 @@ sudo modprobe uvcvideo nodrop=1 timeout=10000 quirks=0x80
 export VERBOSE=5
 export BEHOST="192.168.1.11"
 echo "connecting to " + $BEHOST
-cmd=/home/amirma/seawise-video-streamer/start
+cmd=/home/pi/seawise-video-streamer/start
 $cmd
 
 now=$(date +"%T")
